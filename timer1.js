60c330f3623d3c12d040ce4ffc746ccb9e362b07
helenohyeah <ouyang.helen@gmail.com>
2020-09-22 20:07:47 +0000

    Turn array of string inputs into an array of numbers (alarmTimes)

--- timer1.js
+++ timer1.js
@@ -5,16 +5,26 @@ const timer = () => {
   const alertBell = () => console.log("\u0007");
   const sortNum = (a, b) => a - b;
 
-  let userInput = process.argv.slice(2); // get user input as array
-  userInput.sort(sortNum);
+  let userInputs = process.argv.slice(2); // get user input as array
+  let alarmTimes = userInputs
+    .map(i => parseInt(i)) // turn string inputs into numbers
+    .filter(i => Boolean(i)) // remove non-numbers
+    .filter(i => i > 0) // remove negative numbers
+    .sort(sortNum);   // sort from smallest to largest
 
-  // sort from smallest to largest
+  console.log(userInputs);
+  console.log(alarmTimes);
+
+
+  // for (let i = 0; i <= Math.max(...userInput); i++) {
+  //   console.log(i);
+  // }
   // loop through a counter that starts from 0 to largest
   // loop through the userInput
   // beep when input === counter
   // else silence??
-  console.log(userInput);
-  alertBell();
+  // console.log(userInput);
+  // alertBell();
 };
 
 timer();
