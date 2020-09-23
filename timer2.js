7d7947e56ad1153909821b5f731570376e7fc8f8
helenohyeah <ouyang.helen@gmail.com>
2020-09-23 02:23:40 +0000

    Updated instructions copy and fixed linting

--- timer2.js
+++ timer2.js
@@ -1,44 +1,27 @@
-// user can input any number from 1-9
-  // immediately output "Setting timer for x seconds..."
-  // beep after x seconds has passed
-// user can CTRL + C to exit
-  // say "Thanks for using me, ciao!" before terminating
-
 const readline = require('readline');
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+const startTimer = (input) => {
 
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding('utf8');
-
-// // TESTING KEY PRESS. Play beep if b is pressed
-// stdin.on('data', (key) => {
-//   // console.log(key);
-//   if (key === 'b') {
-//     process.stdout.write("\u0007");
-//   }
-//   if (key === '\u0003') {
-//     process.exit();
-//   }
-// });
-
-
-const timer = (input) => {
   const printInstructions = () => {
-    console.log('How to use Timer');
-    console.log('----------------');
-    console.log('Enter a number from 1 to 9 to start a timer. A beep will sound once the timer counts down to 0');
-    console.log('Press "b" to sound an beep right away');
-    console.log('Press "Ctrl + C" to exit the program');
-  }
+    console.log('---TO START A TIMER---');
+    console.log('enter a number from 1 to 9 to set a timer (in seconds)');
+    console.log('a beep will sound when the timer counts down to 0');
+    console.log('---TO SOUND A BEEP----');
+    console.log('press b to sound a beep right away');
+    console.log('---TO EXIT------------');
+    console.log('press "Ctrl + C"');
+  };
+
   const soundBell = () => process.stdout.write("\u0007");
-  
-  
+    
   const askForInput = (input) => {
     rl.question(input , (answer) => {
       if (answer > 0 && answer < 10) {
@@ -47,8 +30,8 @@ const timer = (input) => {
         setTimeout(() => {
           // start a timer based on input
           soundBell();
-          askForInput('Enter a number from 1 to 9: '); // after bell sounds, ask for another input
-        }, answer * 1000)
+          askForInput('Enter a number from 1 to 9: '); // after bell sounds, ask for input again
+        }, answer * 1000);
       } else {
         // all other input will reprompt user for a valid input
         askForInput('Invalid entry.\nEnter a number from 1 to 9: '); // recursive case
@@ -59,7 +42,7 @@ const timer = (input) => {
   printInstructions();
   
   stdin.on('data', (key) => {
-    if (key === 'b') { 
+    if (key === 'b') {
       // check for b keypress to sound beep right away
       process.stdout.write("\u0007");
     }
@@ -73,15 +56,4 @@ const timer = (input) => {
   askForInput(input); // ask for number from 1 to 9
 };
 
-  // rl.question('Input: ', (answer) => {
-  //   if (answer === 'b') {
-  //     soundBell();
-  //   } else if (answer > 0 || answer < 10) {
-  //     console.log(`Setting timer for ${answer} seconds...`);
-  //     setTimeout(() => {
-  //       soundBell();
-  //     }, answer * 1000)
-  //   } 
-  // });
-
-timer(`Enter a number from 1 to 9: `);
+startTimer(`Enter a number from 1 to 9: `);
