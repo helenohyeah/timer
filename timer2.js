1ded14f32d1ce2afebaea57038fde9970589a186
helenohyeah <ouyang.helen@gmail.com>
2020-09-23 02:16:29 +0000

    Completed functioning of timer2 assignment

--- timer2.js
+++ timer2.js
@@ -1,4 +1,3 @@
-// user can press b and it should beep right away
 // user can input any number from 1-9
   // immediately output "Setting timer for x seconds..."
   // beep after x seconds has passed
@@ -13,34 +12,76 @@ const rl = readline.createInterface({
 });
 
 
-const timer = () => {
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+// // TESTING KEY PRESS. Play beep if b is pressed
+// stdin.on('data', (key) => {
+//   // console.log(key);
+//   if (key === 'b') {
+//     process.stdout.write("\u0007");
+//   }
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// });
+
+
+const timer = (input) => {
   const printInstructions = () => {
     console.log('How to use Timer');
     console.log('----------------');
     console.log('Enter a number from 1 to 9 to start a timer. A beep will sound once the timer counts down to 0');
-    console.log('Press "b" to sound an beep immediately');
+    console.log('Press "b" to sound an beep right away');
     console.log('Press "Ctrl + C" to exit the program');
   }
-  const alertBell = () => process.stdout.write("\u0007");
-
+  const soundBell = () => process.stdout.write("\u0007");
+  
+  
+  const askForInput = (input) => {
+    rl.question(input , (answer) => {
+      if (answer > 0 && answer < 10) {
+        // check input is between 1 to 9
+        console.log(`Setting timer for ${answer} seconds...`);
+        setTimeout(() => {
+          // start a timer based on input
+          soundBell();
+          askForInput('Enter a number from 1 to 9: '); // after bell sounds, ask for another input
+        }, answer * 1000)
+      } else {
+        // all other input will reprompt user for a valid input
+        askForInput('Invalid entry.\nEnter a number from 1 to 9: '); // recursive case
+      }
+    });
+  };
+  
   printInstructions();
-
-  rl.question('Input: ', (answer) => {
-    if (answer === 'b') {
-      alertBell();
-    } else if (answer > 0 || answer < 10) {
-      console.log(`Setting timer for ${answer} seconds...`);
-      setTimeout(() => {
-        alertBell();
-      }, answer * 1000)
-    } 
+  
+  stdin.on('data', (key) => {
+    if (key === 'b') { 
+      // check for b keypress to sound beep right away
+      process.stdout.write("\u0007");
+    }
+    if (key === '\u0003') {
+      // check for CTRL + C to exit
+      console.log('\nThanks for using me. Ciao!');
+      process.exit();
+    }
   });
-
-  rl.on('SIGINT', () => {
-    console.log('Thanks for using me. Ciao!');
-    rl.close();
-  });
-
+  
+  askForInput(input); // ask for number from 1 to 9
 };
 
-timer();
+  // rl.question('Input: ', (answer) => {
+  //   if (answer === 'b') {
+  //     soundBell();
+  //   } else if (answer > 0 || answer < 10) {
+  //     console.log(`Setting timer for ${answer} seconds...`);
+  //     setTimeout(() => {
+  //       soundBell();
+  //     }, answer * 1000)
+  //   } 
+  // });
+
+timer(`Enter a number from 1 to 9: `);
